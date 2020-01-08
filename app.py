c5a1b32446631328401a8a972392e8a98f62afbe
mandargogate <mandargogate@outlook.com>
2020-01-08 00:44:18 -0500

    added load method

--- app.py
+++ app.py
@@ -3,12 +3,36 @@
 
 from flask import Flask, jsonify, render_template, flash
 import dao as db
+import webScrapPopulation as webscrap
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
-    return "choose /city or /county"
+    htmlTag ="""<html>
+                <table>
+                
+                    <tr>
+                        <td> To get city json - use </td>
+                        <td> /city </td>
+                    </tr>
+                    <tr>
+                        <td> To get county json - use </td>
+                        <td> /county </td>
+                    </tr>
+                    <tr>
+                        <td> To load city json - use </td>
+                        <td> /loadCity </td>
+                    </tr>
+                    <tr>
+                        <td> To load county json - use </td>
+                        <td> /loadCounty </td>
+                    </tr>
+                
+                <table>
+                <html>
+            """
+    return htmlTag
 
 @app.route("/city")
 def cityPopulation():
@@ -17,6 +41,16 @@ def cityPopulation():
 @app.route("/county")
 def countyPopulation():
     return db.getCountyPopulation()
+
+@app.route("/loadCounty")
+def loadCountyPopulation():
+    webscrap.populationByCounties()
+    return "County data loaded successfully.."
+
+@app.route("/loadCity")
+def loadCityPopulation():
+    webscrap.populationByCities()
+    return "City data loaded successfullly.."
 
 if __name__ == "__main__":
     app.run(debug=True)
